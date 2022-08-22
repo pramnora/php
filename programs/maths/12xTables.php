6bf6603960178d676a164018a21f14e47101275b
Paul Ramnora <pramnora@yahoo.com>
2022-08-22 16:43:47 +0100

    Create 12xTables.php
    Adde file: [12xTables.php] Prints out the selected number time tables.

---
+++ programs/maths/12xTables.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+  <meta charset="utf-8">
+  <title>PHP Test</title>
+ </head>
+  <body>
+    <?php 
+    # *** PROGRAM: 12 x Tables...
+    $tablesNo = 7;
+    for ($timesNo = 1; $timesNo < 13; $timesNo++){
+       echo $timesNo. " X ".$tablesNo. " = ". $timesNo * $tablesNo."<br>";
+    }
+    ?> 
+  </body>
+</html>
